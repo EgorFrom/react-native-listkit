f45d5f0cb5f2f50f2c46cd6afab6d954e56e81ec
nagibin <e.s.nagibin@tmn3.etagi.com>
2020-02-08 18:45:09 +0500

    feature/ImageSwiper v0 draft to cocoapod

---
+++ ios/RNSwiper/RNLGridViewManager.m
@@ -0,0 +1,20 @@
+#import <React/RCTViewManager.h>
+#import "RNSwiper-Swift.h"
+
+@interface RNLGridViewManager : RCTViewManager
+@end
+
+@implementation RNLGridViewManager
+
+RCT_EXPORT_MODULE()
+// TODO: does this need to be custom?
+RCT_CUSTOM_VIEW_PROPERTY(sections, NSArray<NSDictionary>, RNLGridView) {
+  view.sections = [RCTConvert NSArray:json];
+}
+RCT_EXPORT_VIEW_PROPERTY(onSectionEndReached, RCTDirectEventBlock);
+
+- (UIView *)view {
+  return [[RNLGridView alloc] initWithBridge:self.bridge];
+}
+
+@end
